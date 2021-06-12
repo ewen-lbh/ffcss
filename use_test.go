ff458f8c14f8025cffeb991fc9b00ec3329aec91
Ewen Le Bihan <hey@ewen.works>
2021-06-12 10:36:23 +0200

    ♻️ Refactor tests

--- use_test.go
+++ use_test.go
@@ -3,50 +3,58 @@ package main
 import (
 	"net/url"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResolveThemeName(t *testing.T) {
 	name, typ := ResolveThemeName("ewen-lbh/ffcss")
-	if "https://github.com/ewen-lbh/ffcss" != name || typ != "git" {
-		t.Errorf(`ewen-lbh/ffcss
-want: (%s, %s)
-got:  (%s, %s)`, "https://github.com/ewen-lbh/ffcss", "git", name, typ)
-	}
+	assert.Equal(t, []string{name, typ}, []string{"https://github.com/ewen-lbh/ffcss", "git"})
 
 	name, typ = ResolveThemeName("bitbucket.io/guaca/mole")
-	if "https://bitbucket.io/guaca/mole" != name || typ != "website" {
-		t.Errorf(`bitbucket.io/guaca/mole
-want: (%s, %s)
-got:  (%s, %s)`, "https://bitbucket.io/guaca/mole", "website", name, typ)
-	}
+	assert.Equal(t, []string{name, typ}, []string{"https://bitbucket.io/guaca/mole", "website"})
 
 	name, typ = ResolveThemeName("http://localhost:8080/")
-	if "http://localhost:8080/" != name || typ != "website" {
-		t.Errorf(`http://localhost:8080/
-want: (%s, %s)
-got:  (%s, %s)`, "http://localhost:8080/", "website", name, typ)
-	}
+	assert.Equal(t, []string{name, typ}, []string{"http://localhost:8080/", "website"})
 
 	name, typ = ResolveThemeName("materialfox")
-	if "https://github.com/muckSponge/MaterialFox" != name || typ != "git" {
-		t.Errorf(`materialfox
-want: (%s, %s)
-got:  (%s, %s)`, "https://github.com/muckSponge/MaterialFox", "git", name, typ)
-	}
+	assert.Equal(t, []string{name, typ}, []string{"https://github.com/muckSponge/MaterialFox", "git"})
 
 	name, typ = ResolveThemeName("unknownone")
-	if "" != name || typ != "" {
-		t.Errorf(`unknownone
-want: (%s, %s)
-got:  (%s, %s)`, "", "", name, typ)
-	}
+	assert.Equal(t, []string{name, typ}, []string{"", ""})
 }
 
-func TestDownloadRepository(t *testing.T) {
+func TestGetThemeDownloadPath(t *testing.T) {
 	URL, _ := url.Parse("https://github.com/muckSponge/MaterialFox")
-	clonedTo, err := DownloadRepository(*URL)
-	if err != nil {
-		t.Fatal(err.Error())
+	assert.Equal(t, GetThemeDownloadPath(*URL), GetConfigDir()+"/themes/@muckSponge/MaterialFox")
+
+	URL, _ = url.Parse("https://github.com/users/schoolsyst")
+	assert.Equal(t, GetThemeDownloadPath(*URL), GetConfigDir()+"/themes/-github.com/users/schoolsyst")
+
+	URL, _ = url.Parse("https://ewen.works/")
+	assert.Equal(t, GetThemeDownloadPath(*URL), GetConfigDir()+"/themes/-ewen.works")
+
+	URL, _ = url.Parse("http://localhost:8080")
+	assert.Equal(t, GetThemeDownloadPath(*URL), GetConfigDir()+"/themes/-localhost:8080")
+
+}
+
+func TestIsURLClonable(t *testing.T) {
+	var actual bool
+	var err error
+
+	actual, err = IsURLClonable(urlOf("https://github.com/ewen-lbh/ffcss/"))
+	assert.Equal(t, actual, true)
+	assert.Nil(t, err)
+
+	actual, err = IsURLClonable(urlOf("https://github.com/users/schoolsyst"))
+	assert.Equal(t, actual, false)
+	assert.Nil(t, err)
+
+	actual, err = IsURLClonable(urlOf("https://ewen.works/"))
+	assert.Equal(t, actual, false)
+	assert.Nil(t, err)
+}
+
 	}
 	Assert(t,
 		clonedTo,
