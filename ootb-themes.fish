fe6cf13b7822e4799fa71c7635a70fb2797deedd
Ewen Le Bihan <hey@ewen.works>
2021-07-10 10:17:37 +0200

    🔨 ootb-themes.fish: Include 'by [link to github user]'

--- ootb-themes.fish
+++ ootb-themes.fish
@@ -3,5 +3,6 @@
 # generates markdown markup to be used in README.md/whatever.
 
 for theme in themes/*.yaml
-	echo - [(yq --raw-output .name < $theme)]\((yq --raw-output .download < $theme)\)
+	set username (yq --raw-output .download < $theme | rev | cut -d/ -f2 | rev)
+	echo - [(yq --raw-output .name < $theme)]\((yq --raw-output .download < $theme)\) by [$username]\(https://github.com/$username\)
 end
