3f1e29a434c045c597ed0a1f5c755cc03a2f6a5e
Ewen Le Bihan <hey@ewen.works>
2021-07-31 15:59:30 +0200

    🚸 cache clear: re-create empty cache directory after deleting

--- cache.go
+++ cache.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // CleanDownloadArea removes the temporary download area used to download themes before knowing their name from their manifest
 func CleanDownloadArea() error {
@@ -9,5 +12,15 @@ func CleanDownloadArea() error {
 
 // ClearWholeCache destroys the cache directory
 func ClearWholeCache() error {
-	return os.RemoveAll(CacheDir())
+	err := os.RemoveAll(CacheDir())
+	if err != nil {
+		return fmt.Errorf("while deleting directory tree %q: %w", CacheDir(), err)
+	}
+
+	err = os.Mkdir(CacheDir(), 0777)
+	if err != nil {
+		return  fmt.Errorf("while re-creating empty cache directory at %q: %w", CacheDir(), err)
+	}
+
+	return nil
 }
