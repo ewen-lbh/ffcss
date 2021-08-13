85b5f27a346497593adfcb92294e2e0f22d894a8
Ewen Le Bihan <hey@ewen.works>
2021-08-13 23:24:59 +0200

    🔨 Improve & fix tools/untested

--- tools/untested.rb
+++ tools/untested.rb
@@ -5,11 +5,12 @@ untested = []
 
 files = Dir.glob "*.go"
 testfiles = Dir.glob "*_test.go"
+function_pattern = /^func\s+(\(\s*\w+\s+(?<receiver>[^)\s]+)\s*\)\s+)?\s*(?<name>\w+).+\{$/
 
 testfiles.each do |file|
   lines = Pathname.new(file).read.split "\n"
   lines.each do |line|
-    match = line.match /^func\s+(\([^\)]+\)\s+)?\s*(?<name>\w+)\s*\([^\)]*\).+\{$/
+    match = line.match function_pattern
     if match != nil and match[:name].start_with? "Test"
       tested << match[:name].delete_prefix("Test")
     end
@@ -19,11 +20,12 @@ end
 files.each do |file|
   lines = Pathname.new(file).read.split "\n"
   lines.each_with_index do |line, i|
-    match = line.match /^func\s+(\s*(?<receiver>\w+)\s+.+\)\s+)?\s*(?<name>\w+)\s*\([^\)]*\).+\{$/
+    match = line.match function_pattern
     if match != nil and not match[:name].start_with? "Test" and match[:name] !~ /main|init/
       if not tested.find { |func| func.downcase == match[:name].downcase } and not file.end_with? "_test.go"
         untested << {
-          name: (match[:receiver] || "") + match[:name],
+          name: (if match[:receiver] then "(#{match[:receiver]})." else "" end) + match[:name],
+          # name: match[:name],
           location: "#{file}:#{i + 1}",
         }
       end
@@ -46,9 +48,14 @@ if untested.size == 0
 end
 
 untested.each do |func|
-  puts "untested: #{align untested, func, :name, right: true} @ #{align untested, func, :location}"
+  puts (align untested, func, :location) + " "  + (align untested, func, :name)
 end
 
-puts "#{untested.size} function(s)"
+puts
+puts (if untested.size > 1
+  "#{untested.size} functions"
+else
+  "one function"
+end) + ", " + (Float(untested.size) / (untested.size + tested.size) * 100).round.to_s + "%"
 
 exit 1
